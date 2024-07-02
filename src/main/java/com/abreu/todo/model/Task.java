b0d2c776b5d30166ef0c0a2a21e1387b5b3897b8
Igor Nascimento <igor.nacimento51@gmail.com>
2024-07-02 15:48:39 -0300

    feat: creating entity

---
+++ src/main/java/com/abreu/todo/model/Task.java
@@ -0,0 +1,29 @@
+package com.abreu.todo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private boolean done;
+}
